6e63eaf4c3f3600510472df0133165d63ffa7375
Arunas555 <138851316+Arunas555@users.noreply.github.com>
2024-03-18 00:06:58 -0700

    Update ABC.java

--- src/com/demopkg/ABC.java
+++ src/com/demopkg/ABC.java
@@ -5,6 +5,9 @@ public class ABC {
 	public static void main(String[] args) {
 
 		  System.out.println("Hello world!");
+		  System.out.println(" Selenium java!");
+		  System.out.println(" Hello git!");
+
 	}
 
 }
